d546c12ead4cade070d5170b042bb0034b44cc65
Ifeoluwakareem <Ifeoluwakareem100@gmail.com>
2026-04-16 09:25:57 +0100

    ML recent commit

--- Python_basics_001/shalom_practice.py
+++ Python_basics_001/shalom_practice.py
@@ -85,17 +85,52 @@ def gender_identity(identify):
 #     gender_identity(genders_array[k])
     # k += 1
     
-genders_array = ["Gay", "Straight", "Female"]
+# genders_array = ["Gay", "Straight", "Female"]
 
-for i in genders_array:
-    print(f"I am working on {i}")
-    gender_identity(i)
+# for i in genders_array:
+#     print(f"I am working on {i}")
+#     gender_identity(i)
     
-j = 0
-while j < len(genders_array):
-   print(f"I am working on {genders_array[j]}")  
-   gender_identity(genders_array[j])  
-   j += 1
+# j = 0
+# while j < len(genders_array):
+#    print(f"I am working on {genders_array[j]}")  
+#    gender_identity(genders_array[j])  
+#    j += 1
+    
+# family = ["Father", "Mother", "Sister", "Uncle", "Aunt", "Nephew", "Niece"]
+# for n in family:
+#     print(n)
+    
+# j = 0
+# while j < len(family):
+#     print(family[j])
+#     j += 1
     
     
-    
+  
+# class FullName: #create a class called FullName
+     
+#      def __init__(self, name, dog): #special objects that runs when yoy create an object, takes in name and dog
+#          self.name = name #saves the name inside the object
+#          self.dog = dog #saves the dog's name inside the object
+         
+#      def namet(self): #defines a method (function inside class)
+#          ret_name = f"My name is {self.name}" #create a sentence using the name
+#          print(ret_name) #display the sentence
+#          return ret_name #send the sentence back when the method is called
+     
+#      def name_dog(self): #defines a method 
+#          ret_name = f"My name is {self.name} and my dog's name is {self.dog}" #create a sentence with name and dog
+#          print(ret_name) #display it
+#          return ret_name #return it
+
+
+# FullNameInstance = FullName("Mork", "Pecky")  #create an object from the class Mork as name, Pecky as dog  
+# my_name = FullNameInstance.namet() #call the first method, save the result in my_name
+# my_name_dog = FullNameInstance.name_dog() #call the second method, save the result in my_name_dog
+
+# name_name = FullNameInstance.name #get the name directly from the object
+# print(name_name) #print the name
+
+# dog_name = FullNameInstance.dog #get the dog's name
+# print(dog_name) #print the dog's name    
