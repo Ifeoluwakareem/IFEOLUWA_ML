cf72cc058a5ca781d73cfab4a15cfad4c854bee5
Ifeoluwakareem <Ifeoluwakareem100@gmail.com>
2026-04-20 13:06:36 +0100

    Morphism

--- Python_basics_01/OOP.py
+++ Python_basics_01/OOP.py
@@ -19,50 +19,82 @@
 
 #class attribute
 
-class BeautyFinder:
+# class BeautyFinder:
     
-    # general class attributes
+#     # general class attributes
     
-    long_legs = True
-    long_nose = True
+#     long_legs = True
+#     long_nose = True
     
-    #Instance attribute
-    def __init__(self, name, height, weight):
-        self.name = name
-        self.height = height
-        self.weight = weight
+#     #Instance attribute
+#     def __init__(self, name, height, weight):
+#         self.name = name
+#         self.height = height
+#         self.weight = weight
         
-    def detect_beauty(self):
-        if int(self.height) >= 20:
-            print(f"Model {self.name} is very beautiful.")
-        else:
-            print(f"Model {self.name} is not beautiful.")
+#     def detect_beauty(self):
+#         if int(self.height) >= 20:
+#             print(f"Model {self.name} is very beautiful.")
+#         else:
+#             print(f"Model {self.name} is not beautiful.")
             
-    def detect_obesity(self):
-        if int(self.weight) >= 30:
-            print(f"Model {self.name} is obese.")
-        else:
-            print(f"Model {self.name} is not obese.") 
+#     def detect_obesity(self):
+#         if int(self.weight) >= 30:
+#             print(f"Model {self.name} is obese.")
+#         else:
+#             print(f"Model {self.name} is not obese.") 
             
-class Gender(BeautyFinder):
-    male = "MALE"
-    female = "FEMALE"
+# class Gender(BeautyFinder):
+#     male = "MALE"
+#     female = "FEMALE"
     
-olawale = BeautyFinder("olawale", 10, 50) 
-# print(
-#     olawale.long_legs,
-#     olawale.long_nose,
-#     olawale.name,
-#     olawale.height,
-#     olawale.weight
-#     )
+# olawale = BeautyFinder("olawale", 10, 50) 
+# # print(
+# #     olawale.long_legs,
+# #     olawale.long_nose,
+# #     olawale.name,
+# #     olawale.height,
+# #     olawale.weight
+# #     )
 
-ife = BeautyFinder("Ife", 50, 20)
+# ife = BeautyFinder("Ife", 50, 20)
 
 # ife.detect_beauty()
 # ife.detect_obesity()
 
-gg = Gender("Shalom", 60, 10)
-print(gg.male, gg.female)
-gg.detect_beauty()
-gg.detect_obesity()
+# gg = Gender("Shalom", 60, 10)
+# print(gg.male, gg.female)
+# gg.detect_beauty()
+# gg.detect_obesity()
+
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
+
+    def make_sound(self):
+        print("Meow")
+
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
+
+    def make_sound(self):
+        print("Bark")
+
+
+cat1 = Cat("Kitty", 2.5)
+dog1 = Dog("Fluffy", 4)
+
+for animal in (cat1, dog1):
+    animal.make_sound()
+    animal.info()
+    animal.make_sound()
