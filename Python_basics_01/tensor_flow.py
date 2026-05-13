4710c09ee911e85e26a89089557f9b6a286e1b91
Ifeoluwakareem <Ifeoluwakareem100@gmail.com>
2026-05-13 18:23:22 +0100

    tensorflow

---
+++ Python_basics_01/tensor_flow.py
@@ -0,0 +1,10 @@
+ #In this notebook,we are going to cover some of the most fundamental concepts of tensors using TensorFlow
+
+#  More specifically, we are going to cover:
+#  *Introduction to tensors__
+#  *Getting information from tensorstor
+#  *Manipulating tensors
+#  *Tensors and NumPy
+#  *Using @tf.function (a way to speed up your regular python functions)
+#  *Using GPUs with tensorFlow (or TPUs)
+#  *Exercises to try for yourself
